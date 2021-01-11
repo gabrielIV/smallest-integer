f6028c4992d9efad720e935890a4c3a006d61f9b
gabrielIV <slicebizz@gmail.com>
2021-01-11 21:57:34 +0300

    added 123 test case

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 findSmallest = (arr) => {
   var smallest;
+  //   sort array
   arr.sort();
 
+  //loop through array
   for (var i = 0; i < arr.length; i++) {
+    //   find difference between adjascent array items
+    // if difference > 1 smallest number in array is found
     if (i && arr[i] - arr[i - 1] > 1 && arr[i - 1] + 1 > 0) {
       smallest = arr[i - 1] + 1;
       break;
     }
   }
-  return smallest || 1;
+  if (smallest) return smallest;
+  return arr[arr.length - 1] > 0 ? arr[arr.length - 1] + 1 : 1;
 };
 
-console.log(findSmallest([3, 2, 2, 5, 6, 8, -2, -3]));
+console.log(findSmallest([1, 2, 3]));
