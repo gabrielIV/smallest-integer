d8c75a1e19fcae43a8ffa46be5828c47fff2df11
gabrielIV <slicebizz@gmail.com>
2021-01-11 22:02:25 +0300

    added 123 test case

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ findSmallest = (arr) => {
 
   //loop through array
   for (var i = 0; i < arr.length; i++) {
-    //   find difference between adjascent array items
-    // if difference > 1 smallest number in array is found
+    // find difference between adjascent array items
+    // if difference > 1 smallest number not in array is found
     if (i && arr[i] - arr[i - 1] > 1 && arr[i - 1] + 1 > 0) {
       smallest = arr[i - 1] + 1;
       break;
