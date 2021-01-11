46ef304394dc81c3d4c45f7f6981de6944c709c6
gabrielIV <slicebizz@gmail.com>
2021-01-11 23:24:13 +0300

    added single value support

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-findSmallest = (arr) => {
-  var smallest;
-  //   sort array
-  arr.sort();
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
 
+function solution(A) {
+  var arr = A;
+  var smallest = null;
+  //   sort array
+  arr.sort((a, b) => a - b);
+
+  if (arr[0] > 1) return 1;
+
+  var count = 0;
+  let len = arr.length;
   //loop through array
   for (var i = 0; i < arr.length; i++) {
+    count++;
     // find difference between adjascent array items
     // if difference > 1 smallest number not in array is found
     if (i && arr[i] - arr[i - 1] > 1 && arr[i - 1] + 1 > 0) {
@@ -12,8 +21,14 @@ findSmallest = (arr) => {
       break;
     }
   }
-  if (smallest) return smallest;
-  return arr[arr.length - 1] > 0 ? arr[arr.length - 1] + 1 : 1;
-};
 
-console.log(findSmallest([1, 2, 3]));
+  if (!smallest && count === len && len > 1) smallest = arr[len - 1] + 1;
+
+  if (len === 1) smallest = arr[0] - 1;
+
+  if (smallest < 1) return 1;
+
+  return smallest;
+}
+
+console.log(solution([1]));
